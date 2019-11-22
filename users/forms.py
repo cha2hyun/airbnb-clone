92d1a45cb215889c1f2f5563b983d855524eed08
cha2hyun_dev <soohyun5939@icloud.com>
2019-11-22 23:12:20 +0900

    SignUp

--- users/forms.py
+++ users/forms.py
@@ -17,3 +17,29 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not Exist"))
+
+
+class SignUpForm(forms.ModelForm):
+
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email", "birthdate")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
--- users/models.py
+++ users/models.py
@@ -35,6 +35,6 @@ class User(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICIES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICIES, max_length=3, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICIES, max_length=2, blank=True, default="kr")
+    currency = models.CharField(choices=CURRENCY_CHOICIES, max_length=3, blank=True, default="krw")
     superhost = models.BooleanField(default=False)
