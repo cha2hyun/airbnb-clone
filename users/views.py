53508e6f23f62ffac3237b5b5ec145b95ecc59f4
cha2hyun_dev <soohyun5939@icloud.com>
2019-11-25 23:27:23 +0900

    Login Logout complete

--- users/views.py
+++ users/views.py
@@ -33,11 +33,11 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "Soo Hyun",
-        "last_name": "Chae",
-        "email": "cha2hyun.dev@gmail.com",
-    }
+    # initial = {
+    #     "first_name": "Soo Hyun",
+    #     "last_name": "Chae",
+    #     "email": "cha2hyun.dev@gmail.com",
+    # }
 
     def form_valid(self, form):
         form.save()
