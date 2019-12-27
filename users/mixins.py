4b0b4ed00814b9de7789e2595e96bf701a72fc97
cha2hyun_dev <soohyun5939@icloud.com>
2019-12-27 15:27:58 +0900

    #21.10 Great User Mixins

--- users/mixins.py
+++ users/mixins.py
@@ -1,13 +1,28 @@
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
+
+class EmailLoginOnlyView(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.login_method == "email"
+
+    def handle_no_permission(self):
+        messages.error(self.request, f"{self.request.user.login_method} login can't change the password")
+        return redirect("core:home")
 
 
 class LoggedOutOnlyView(UserPassesTestMixin):
-
-    permission_denied_message = "Page not found"
 
     def test_func(self):
         return not self.request.user.is_authenticated
 
     def handle_no_permission(self):
+        messages.error(self.request, "logout first please")
         return redirect("core:home")
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+
+    login_url = reverse_lazy("users:login")
