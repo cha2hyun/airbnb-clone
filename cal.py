7098fc557a4dfe67adb7e7f530e8334a4b3c941d
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2019-12-31 16:31:14 +0900

    #24.1 Room Calendar part Two

--- cal.py
+++ cal.py
@@ -1,12 +1,13 @@
 import calendar
 
 
-class Calendar:
+class Calendar(calendar.Calendar):
 
     def __init__(self, year, month):
+        super().__init__(firstweekday=6)
         self.year = year
         self.month = month
-        self.day_names = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+        self.day_names = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
         self.months = (
             "January",
             "February",
@@ -21,6 +22,14 @@ class Calendar:
             "November",
             "December",
         )
+
+    def get_days(self):
+        weeks = self.monthdays2calendar(self.year, self.month)
+        days = []
+        for week in weeks:
+            for day, week_day in week:
+                days.append(day)
+        return days
 
     def get_month(self):
         return self.months[self.month - 1]
