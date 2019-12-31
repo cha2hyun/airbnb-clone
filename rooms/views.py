10ba0656a4c8ef0fbc1d5d84d099048b42f35f31
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2019-12-31 15:51:38 +0900

    #23.9 Creating Room part Two

--- rooms/views.py
+++ rooms/views.py
@@ -200,5 +200,6 @@ class CreateRoomView(user_mixins.LoggedInOnlyView, FormView):
         room = form.save()
         room.host = self.request.user
         room.save()
-        messages.success(self.request, "Photo Uploaded")
+        form.save_m2m()
+        messages.success(self.request, "Room Created")
         return redirect(reverse("rooms:detail", kwargs={"pk": room.pk}))
