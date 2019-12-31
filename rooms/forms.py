c49a905e9775da98b89000f7a0f48adb465f0690
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2019-12-31 11:56:12 +0900

    #23.5 Add Photo part One

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django_countries.fields import CountryField
 from . import models
+
+
 class SearchForm(forms.Form):
-    
+
     city = forms.CharField(initial="Anywhere")
     country = CountryField(default="KR").formfield()
     price = forms.IntegerField(required=False)
@@ -15,3 +17,15 @@ class SearchForm(forms.Form):
     superhost = forms.BooleanField(required=False)
     amenities = forms.ModelMultipleChoiceField(required=False, queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple)
     facilities = forms.ModelMultipleChoiceField(required=False, queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+
+class CreatePhotoForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Photo
+        fields = ("caption",  "file")
+
+    def save(self, pk, *args, **kwargs):
+        photo = super().save(comit=False)
+        models.Room.objects.ge(pk=pk)
+        photo.room =
