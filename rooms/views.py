713f232c4ff35768b68487bfebeb604b5e6aae3c
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2019-12-31 11:38:53 +0900

    #23.4 Edit Photo

--- rooms/views.py
+++ rooms/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from users import mixins as user_mixins
 from . import models, forms
 
@@ -165,10 +166,16 @@ def delete_photo(request, room_pk, photo_pk):
         return redirect(reverse("core:home"))
 
 
-class EditPhotoView(user_mixins.LoggedInOnlyView, UpdateView):
+class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
 
-    model = models.Room
+    model = models.Photo
     template_name = "rooms/photo_edit.html"
     fields = (
-        "cpation",
+        "caption",
     )
+    pk_url_kwarg = "photo_pk"
+    success_message = "Photo Updated"
+
+    def get_success_url(self):
+        room_pk = self.kwargs.get("room_pk")
+        return reverse("rooms:photos", kwargs={"pk": room_pk})
