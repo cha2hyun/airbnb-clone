2fafe3e2aea3251568e7e81d9a64b24d7f96c98a
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-06 14:45:11 +0900

    #24.16 Validating Everything

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,6 +3,12 @@ from . import models
 
 
 class CreateReviewForm(forms.ModelForm):
+    accuracy = forms.IntegerField(max_value=5, min_value=1)
+    communication = forms.IntegerField(max_value=5, min_value=1)
+    cleanLiness = forms.IntegerField(max_value=5, min_value=1)
+    location = forms.IntegerField(max_value=5, min_value=1)
+    check_in = forms.IntegerField(max_value=5, min_value=1)
+    value = forms.IntegerField(max_value=5, min_value=1)
 
     class Meta:
         model = models.Review
--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from core import models as core_models
 # Create your models here.
 
@@ -8,12 +9,13 @@ class Review(core_models.TimeStampedModel):
     """Review Model Definition"""
 
     review = models.TextField()
-    accuracy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanLiness = models.IntegerField()
-    location = models.IntegerField()
-    check_in = models.IntegerField()
-    value = models.IntegerField()
+
+    accuracy = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    communication = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    cleanLiness = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    location = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    check_in = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     user = models.ForeignKey(
         "users.User", related_name="reviews", on_delete=models.CASCADE
     )
