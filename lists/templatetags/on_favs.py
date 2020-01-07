66ed4b1538055093ea50e37de739405296e0010a
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-07 11:58:11 +0900

    #25.5 Fav Template Tag

---
+++ lists/templatetags/on_favs.py
@@ -0,0 +1,10 @@
+from django import template
+from lists import models as list_models
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def on_favs(context, room):
+    user = context.request.user
+    the_list = list_models.List.objects.get_or_none(user=user, name="My Favorites House")
+    return room in the_list.rooms.all()
