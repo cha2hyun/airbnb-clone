49b0c2d2c7af7c205eca134662d1cc79ea4a72ca
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-08 12:20:02 +0900

    #26.8 Staticfiles

--- config/settings.py
+++ config/settings.py
@@ -194,7 +194,7 @@ if not DEBUG:
     AWS_BUCKET_ACL = 'public-read'
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
