675eaed466036e7029bab17acc0290937540350d
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-08 15:01:13 +0900

    seeding in aws

--- users/management/commands/seed_users.py
+++ users/management/commands/seed_users.py
@@ -8,15 +8,11 @@ class Command(BaseCommand):
     help = "This command creates many user"
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            "--number", default=1, type=int, help="How many users do you want to create")
+        parser.add_argument("--number", default=1, type=int, help="How many users do you want to create")
 
     def handle(self, *args, **options):
         number = options.get("number")
         seeder = Seed.seeder()
-        seeder.add_entity(User, number, {
-            "is_staff": False,
-            "is_superuser": False,
-        })
+        seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False})
         seeder.execute()
         self.stdout.write(self.style.SUCCESS(f"{number} users created!"))
