8c057efebf043ba7e3dbba605716706ca0dcf050
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-20 12:22:19 +0900

    new AWS

--- config/settings.py
+++ config/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET", "wLv2Ty]>29YG<9zNnu<n")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(os.environ.get("DEBUG"))
-DEBUG = os.environ.get("DEBUG") == "True"
-# DEBUG = True
+# DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com", "127.0.0.1"]
 
