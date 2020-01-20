4bdaa06003b61dad7405cad4da3e09a22470842d
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-20 15:08:56 +0900

    new aws setting

--- config/settings.py
+++ config/settings.py
@@ -185,18 +185,18 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 # Sentry
 if not DEBUG:
 
-    # DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorage'
-    # STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
-    DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorage'
+    STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
+    # DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+    # STATICFILES_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_KEY")
     AWS_STORAGE_BUCKET_NAME = 'airbnb-clone-cha2hyun.dev'
     AWS_AUTO_CREATE_BUCKET = True
     AWS_BUCKET_ACL = 'public-read'
-    # AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-    # AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    # STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
