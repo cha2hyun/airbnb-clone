2a899878fc15d0e0648b6094e8dbc478c6675270
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-20 15:16:57 +0900

    new aws setting

--- config/settings.py
+++ config/settings.py
@@ -191,7 +191,7 @@ if not DEBUG:
     # STATICFILES_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_KEY")
-    AWS_STORAGE_BUCKET_NAME = 'airbnb-clone-cha2hyun.dev'
+    AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-ap-northeast-2-329078347595'
     AWS_AUTO_CREATE_BUCKET = True
     AWS_BUCKET_ACL = 'public-read'
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
